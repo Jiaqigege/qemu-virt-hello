df689f37dd70105f1605091d4603e6e890b493d5
GoodBoy <GoodBoy@123.com>
2024-08-20 14:52:12 +0800

    chore: Add dts generate method to tool.sh .

--- README.md
+++ README.md
@@ -2,15 +2,6 @@
 
 A helloworld kernel that runs on QEMU [virt](https://www.qemu.org/docs/master/system/arm/virt.html) machine.
 
-# virt.dts
-
-The virt.dts file is generated using QEMU tools and is intended solely for inspecting the hardware resources. It is not actually used in the system's operation. 
-
-```shell
-qemu-system-aarch64 -machine virt,dumpdtb=virt.dtb -cpu cortex-a57 -smp 1 -m 2G -nographic
-dtc -I dtb -O dts -o virt.dts virt.dtb
-```
-
 ## Credit
 
 Some code is from ChCore, which is the lab of [MOSPI](https://ipads.se.sjtu.edu.cn/mospi/).
--- tool.sh
+++ tool.sh
@@ -29,15 +29,26 @@ debug() {
     qemu-system-aarch64 -machine virt -cpu cortex-a57 -smp 1 -m 2G -kernel build/kernel.img -nographic -s -S
 }
 
+export_dts() {
+    # The virt.dts file is generated using QEMU tools and is intended solely for inspecting the hardware resources. It is not actually used in the system's operation.
+    echo "Exporting DTS from DTB..."
+    qemu-system-aarch64 -machine virt,dumpdtb=virt.dtb -cpu cortex-a57 -smp 1 -m 2G -nographic
+    dtc -I dtb -O dts -o virt.dts virt.dtb
+    rm virt.dtb
+    echo "DTS file generated: virt.dts"
+    exit 0
+}
+
 help() {
-    echo "Usage: $0 {build|b|run|r|clean|c|debug|d|help|h}"
+    echo "Usage: $0 {build|b|run|r|clean|c|debug|d|export_dts|e|help|h}"
     echo
     echo "Commands:"
-    echo "  build (b)   Compile the kernel image (default action)"
-    echo "  run (r)     Run the kernel using QEMU"
-    echo "  clean (c)   Clean the build directory"
-    echo "  debug (d)   Run the kernel in QEMU with debugging options (-s -S)"
-    echo "  help (h)    Display this help message"
+    echo "  build (b)        Compile the kernel image (default action)"
+    echo "  run (r)          Run the kernel using QEMU"
+    echo "  clean (c)        Clean the build directory"
+    echo "  debug (d)        Run the kernel in QEMU with debugging options (-s -S)"
+    echo "  export_dts (e)   Export DTS from the generated DTB"
+    echo "  help (h)         Display this help message"
     exit 0
 }
 
@@ -51,6 +62,7 @@ if [ $# -gt 0 ]; then
         r) action="run" ;;
         c) action="clean" ;;
         d) action="debug" ;;
+        e) action="export_dts" ;;
         h) action="help" ;;
         *) action="$1" ;;
     esac
@@ -68,6 +80,9 @@ case "$action" in
         ;;
     debug)
         debug
+        ;;
+    export_dts)
+        export_dts
         ;;
     help)
         help
